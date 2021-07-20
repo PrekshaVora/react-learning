af9f4d8275338c6752f13c889f2fba3dcd2117db
unknown <vora.preksha2001@gmail.com>
2021-07-20 15:00:06 +0530

    add components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import User from "./components/user";
+
+function App() {
+  return (
+  <div>
+    <h1>Users</h1>
+    <User name= "Preksha" description="FSWD"/>
+    <User name= "Shreya" description="FSWD"/>
+    <User name= "prachi" description="FSWD"/>
+    <User name= "priya" description="FSWD"/>
+ 
+    </div>
+  );
+  }
+export default App;
